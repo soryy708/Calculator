4185fa713348d8f38b037c1949058f908bfa5618
Ivan Rubinson <soryy708@gmail.com>
2016-09-09 21:33:09 +0300

    Add some unit tests

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
+//#define DO_UNIT_TESTS // Uncomment to perform unit tests
 #include "equation.h"
 #include <iostream>
 
+#ifdef DO_UNIT_TESTS
+namespace Tests
+{
+	bool assert(Equation equation, float answer)
+	{
+		double result = equation.compute();
+		if (result != answer)
+		{
+			std::cout << "Unit test failed: " << equation.toString() << "=" << result << " != " << answer << std::endl;
+			return false;
+		}
+		return true;
+	}
+}
+#endif
+
 int main(int argc, char* argv[])
 {
+#ifdef DO_UNIT_TESTS
+	if (Tests::assert(Equation("3+3"), 6.0) &&
+		Tests::assert(Equation("5/2"), 2.5) &&
+		Tests::assert(Equation("3+3*2"), 9.0) &&
+		Tests::assert(Equation("(3+3)*2"), 12.0))
+	{
+		std::cout << "Unit tests suceeded." << std::endl;
+	}
+#endif
+
 	std::string input;
 	if (argc < 2)
 	{
